27ce7c89d4f8f222991a757b7933071a9901a7eb
Dave Chan <davechan813@gmail.com>
2018-08-28 01:11:01 -0700

    fix bug of getting same data every time

--- routes/popularity.js
+++ routes/popularity.js
@@ -1,47 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
 const googleTrends = require('google-trends-api');
 
-
-
-var startTime, endTime, keyWord, geoString, resolution;
-
-
-router.post('/', function(req, res, next) {
-
-    startTime = req.body.startTime;
-    endTime = req.body.endTime;
-    keyWord = req.body.keyWord;
-    geoString = req.body.geoString;
-    resolution = req.body.resolution;
-    console.log("hello")
-    console.log(startTime);
-    // startTime : 2011-11-21
-
-    var startDate = new Date(startTime.split('-')[0], startTime.split('-')[1], startTime.split('-')[2]);
-    var endDate = new Date(endTime.split('-')[0], endTime.split('-')[1], endTime.split('-')[2]);
-    
-    next();
-    // now params are ready
-
-    googleTrends.interestByRegion({keyword: keyWord, startTime: startDate, endTime: endDate, geo: geoString})
-.then((res2) => {
-  console.log(keyWord);
-
-  router.post('/', function(req, res3, next) {
-
-
-    console.log('get here');
-    res3.send(res2);
-  });
-})
-.catch((err) => {
-  console.log(err);
-})
-
+router.post('/', function(req, res1) {
+  console.log(req.body);
+  googleTrends.interestByRegion({
+    keyword: req.body.keyword,
+    startTime: new Date(req.body.startTime),
+    endTime: new Date(req.body.endTime),
+    geo: req.body.geo,
+    resolution: req.body.resolution
+  })
+  .then((res2) => {
+    console.log(res2);
+    res1.send(res2);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 });
-
-
 
 module.exports = router;
