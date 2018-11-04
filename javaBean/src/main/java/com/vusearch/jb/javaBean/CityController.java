c2e6680d43d9ab748018aee51ef100a2ac5fda4f
noddinagushpa <potplus@live.com>
2018-11-03 23:17:04 -0700

    minor updates

--- javaBean/src/main/java/com/vusearch/jb/javaBean/CityController.java
+++ javaBean/src/main/java/com/vusearch/jb/javaBean/CityController.java
@@ -102,7 +102,7 @@ public class CityController {
                 System.out.println("Successfully added " + s);
                 try {
 
-                    TimeUnit.SECONDS.sleep(2);
+                    TimeUnit.SECONDS.sleep(10);
                 } catch (Exception e) {
                     continue;
                 }
